440a7db788da8d5fb9909180d42ce4d131c0058e
Lennart <lkarow@online.de>
2023-01-17 13:32:01 +0900

    add button for login/signup

--- components/NavbarView.tsx
+++ components/NavbarView.tsx
@@ -1,22 +1,39 @@
 import Link from "next/link";
+import React, { useState } from "react";
 
 import styles from "../styles/NavbarView.module.css";
 
+import useModal from "../hooks/useModal";
+import AccessModal from "./AccessModal";
+
 const NavbarView = () => {
+  const [isShowingModal, toggleModal] = useModal();
+
+  const handleActivateModal = () => toggleModal();
+
   return (
-    <nav className={styles.navbar}>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/list">List</Link>
-        </li>
-        <li>
-          <Link href="/board">Board</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <AccessModal showModal={isShowingModal} onClose={toggleModal} />
+
+      <nav className={styles.navbar}>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/list">List</Link>
+          </li>
+          <li>
+            <Link href="/board">Board</Link>
+          </li>
+        </ul>
+        <div className={styles.alignRight}>
+          <button className={styles.btn} onClick={handleActivateModal}>
+            Access
+          </button>
+        </div>
+      </nav>
+    </>
   );
 };
 
