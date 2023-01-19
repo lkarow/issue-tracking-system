df65157c75635d3846e93fed9f8dd708c24457db
Lennart <lkarow@online.de>
2023-01-19 20:08:19 +0900

    show snackbar to inform about the successful signup

--- components/SignupForm.tsx
+++ components/SignupForm.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import styles from "../styles/AccessModal.module.css";
 
+import useSnackBar from "../hooks/useSnackBar";
+import SnackBar from "./SnackBar";
+
 type Prop = {
   onClose: any;
 };
@@ -18,6 +21,9 @@ export default function SignupForm({ onClose }: Prop) {
     Password: "",
   });
 
+  const [isShowingSnackBar, toggleSnackBar] = useSnackBar();
+  const [setSnackBarInfo, setSetSnackBarInfo] = useState("");
+
   const handleChange = (e: any) => {
     const { name, value } = e.target;
 
@@ -30,56 +36,69 @@ export default function SignupForm({ onClose }: Prop) {
   const handleSignup = async (e: any) => {
     e.preventDefault();
 
-    await fetch("/api/users", {
+    const response = await fetch("/api/users", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(signupData),
     });
-    onClose();
+    if (response.status === 200) {
+      setSetSnackBarInfo("Created new account");
+      toggleSnackBar();
+      setTimeout(() => {
+        onClose();
+      }, 1500);
+    }
   };
 
   return (
-    <form className={styles.modalForm} onSubmit={(e) => handleSignup(e)}>
-      <fieldset>
-        <label htmlFor="username-input">Username</label>
-        <input
-          name="Username"
-          id="username-input"
-          type="text"
-          placeholder="Username"
-          onChange={(e) => handleChange(e)}
-        />
-      </fieldset>
-      <fieldset>
-        <label htmlFor="username-input">Email</label>
-        <input
-          name="Email"
-          id="email-input"
-          type="text"
-          placeholder="Email"
-          onChange={(e) => handleChange(e)}
-        />
-      </fieldset>
-      <fieldset>
-        <label htmlFor="password-input">Password</label>
-        <input
-          name="Password"
-          id="password-input"
-          type="password"
-          placeholder="Password"
-          onChange={(e) => handleChange(e)}
-        />
-      </fieldset>
-      <div className={styles.row}>
-        <input
-          className={styles.loginBtn}
-          type="submit"
-          value="Sign up"
-          disabled={
-            !signupData.Username || !signupData.Email || !signupData.Password
-          }
-        />
-      </div>
-    </form>
+    <>
+      <SnackBar
+        showSnackBar={isShowingSnackBar}
+        onCloseSnackBar={toggleSnackBar}
+        infoText={setSnackBarInfo}
+      />
+      <form className={styles.modalForm} onSubmit={(e) => handleSignup(e)}>
+        <fieldset>
+          <label htmlFor="username-input">Username</label>
+          <input
+            name="Username"
+            id="username-input"
+            type="text"
+            placeholder="Username"
+            onChange={(e) => handleChange(e)}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="username-input">Email</label>
+          <input
+            name="Email"
+            id="email-input"
+            type="text"
+            placeholder="Email"
+            onChange={(e) => handleChange(e)}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="password-input">Password</label>
+          <input
+            name="Password"
+            id="password-input"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => handleChange(e)}
+          />
+        </fieldset>
+        <div className={styles.row}>
+          <input
+            className={styles.loginBtn}
+            type="submit"
+            value="Sign up"
+            disabled={
+              !signupData.Username || !signupData.Email || !signupData.Password
+            }
+          />
+        </div>
+      </form>
+    </>
   );
 }
