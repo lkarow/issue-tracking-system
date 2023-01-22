d27c49ea9f5019e973e33e741553351fc430d850
Lennart <lkarow@online.de>
2023-01-22 16:06:16 +0900

    optimize fonts with next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,10 @@ import type { AppProps } from "next/app";
 
 import { SessionProvider } from "next-auth/react";
 
+import { Nunito } from "@next/font/google";
+
+const nuntio = Nunito({ subsets: ["latin"] });
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
@@ -10,6 +14,11 @@ export default function App({
   return (
     // Expose the session context at the top level of the app in order to use useSession
     <SessionProvider session={session}>
+      <style jsx global>{`
+        html {
+          font-family: ${nuntio.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
     </SessionProvider>
   );
