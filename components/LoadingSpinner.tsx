8e0df4f4982c2171ef994b5d22d69a6d08d2e0ac
Lennart <lkarow@online.de>
2023-01-26 13:48:03 +0900

    add testid for testing

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import styles from "../styles/LoadingSpinner.module.css";
 
 export default function LoadingSpinner() {
   return (
-    <div className={styles.ldsGrid}>
+    <div className={styles.ldsGrid} data-testid="loading-spinner">
       <div></div>
       <div></div>
       <div></div>
