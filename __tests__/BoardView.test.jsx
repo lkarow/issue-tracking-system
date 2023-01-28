2f717ae5fde0ebefd57ad76c8265969831c2e10b
Lennart <lkarow@online.de>
2023-01-28 14:09:57 +0900

    fix “not wrapped in act” errors

--- __tests__/BoardView.test.jsx
+++ __tests__/BoardView.test.jsx
@@ -1,23 +1,33 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import BoardView from "../components/BoardView";
 import "@testing-library/jest-dom";
 
-jest.mock("next/router", () => ({
-  useRouter: jest.fn(),
-}));
-
 describe("BoardView", () => {
-  beforeEach(() => {
-    fetch.resetMocks();
-  });
-
-  it("renders LoadingSpinner if loading", () => {
+  it("renders four columns", async () => {
     fetch.mockResponse(JSON.stringify([]));
-
     render(<BoardView />);
 
-    const loadingSpinner = screen.getByTestId("loading-spinner");
+    const backlogColumn = await screen.findByText("Backlog");
+    const progressColumn = await screen.findByText("In Progress");
+    const reviewColumn = await screen.findByText("In Review");
+    const doneColumn = await screen.findByText("Done");
+
+    expect(backlogColumn).toBeInTheDocument();
+    expect(progressColumn).toBeInTheDocument();
+    expect(reviewColumn).toBeInTheDocument();
+    expect(doneColumn).toBeInTheDocument();
+  });
+
+  it("renders LoadingSpinner if loading", async () => {
+    fetch.mockResponse(JSON.stringify([]));
+    render(<BoardView />);
+
+    const loadingSpinner = await screen.findByTestId("loading-spinner");
 
     expect(loadingSpinner).toBeInTheDocument();
+    // Otherwise the it block exits before the loading state disappears and data comes back, resulting in a “not wrapped in act” error
+    await waitFor(() => {
+      expect(loadingSpinner).not.toBeInTheDocument();
+    });
   });
 });
--- __tests__/TaskList.test.jsx
+++ __tests__/TaskList.test.jsx
@@ -1,15 +1,27 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import TaskList from "../components/TaskList";
 import "@testing-library/jest-dom";
 
 describe("TaskList", () => {
-  it("renders LoadingSpinner if loading", () => {
+  it("renders renders table", async () => {
     fetch.mockResponse(JSON.stringify([]));
-
     render(<TaskList />);
 
-    const loadingSpinner = screen.getByTestId("loading-spinner");
+    const loadingSpinner = await screen.findByText("Create Task");
 
     expect(loadingSpinner).toBeInTheDocument();
+  });
+
+  it("renders LoadingSpinner if loading", async () => {
+    fetch.mockResponse(JSON.stringify([]));
+    render(<TaskList />);
+
+    const loadingSpinner = await screen.findByTestId("loading-spinner");
+
+    expect(loadingSpinner).toBeInTheDocument();
+    // Otherwise the it block exits before the loading state disappears and data comes back, resulting in a “not wrapped in act” error
+    await waitFor(() => {
+      expect(loadingSpinner).not.toBeInTheDocument();
+    });
   });
 });
